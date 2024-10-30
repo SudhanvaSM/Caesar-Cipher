146cfc5361eb28203dd5572f38acb79163666b39
Sudhanva S M <sudhanva.madhusudhan@gmail.com>
2024-10-30 22:15:34 +0530

    Cipher.py

--- Cipher.py
+++ Cipher.py
@@ -1,8 +1,8 @@
-def encode_or_decode (original_text, shift_amt, direction):
-    alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+def encode_or_decode (original_text, shift_amt, direction):         #funtion to encrypt or decrypt the message
+    alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']        #list to check index of each element in the message
 
     if direction == 'decode':
-        shift_amt *= -1
+        shift_amt *= -1        #multiplying by -1 so that the program decrypts by shifting the index backwards
     elif direction == 'encode':
         shift_amt *= 1
     else:
@@ -10,9 +10,9 @@ def encode_or_decode (original_text, shift_amt, direction):
     cipher_text = ""
 
     for letter in original_text:
-        shifted_position = alphabets.index(letter) + shift_amt
-        shifted_position %= len(alphabets)
-        cipher_text += alphabets[shifted_position]
+        shifted_position = alphabets.index(letter) + shift_amt        #save the new element in message by adding the index of message with specified amount
+        shifted_position %= len(alphabets)                            #to keep the loop in range of length of alphabets list
+        cipher_text += alphabets[shifted_position]                    #append the new element into encrypted/decrypted string
     
     print(f"Here is the {direction}d result: {cipher_text}")
 
